2f09f766ef29e819f0295452521e44f83fc34921
jaquel <jaquelrey@gmail.com>
2022-11-05 15:56:20 -0400

    update: login / signup handler

--- public/js/login.js
+++ public/js/login.js
@@ -28,32 +28,7 @@ const loginHandler = async function (event) {
     alert("Login failed, try again.");
   }
 };
-// to sign up,
-const signupHandler = async function (event) {
-  // prevent reload on form submit
-  event.preventDefault();
-  // get user input for username and password
-  const nameEl = document.querySelector("#name-input-signup");
-  const pwEl = document.querySelector("#pw-input-signup");
-  // send as json to the user route
-  const res = await fetch("/api/user", {
-    method: "POST",
-    body: JSON.stringify({
-      username: nameEl.value,
-      password: pwEl.value,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-  // if signup successful ans promise fulfilled,
-  // redirect user to their dashboard
-  // else, alert user to failure
-  if (res.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    alert("Sign up failed, try again.");
-  }
-};
+
 // click event listener for login form submission
 document.querySelector("#login-button").addEventListener("click", loginHandler);
-// event listener for signup button
-document.querySelector("#signup-button").addEventListener("click", signupHandler);
+
