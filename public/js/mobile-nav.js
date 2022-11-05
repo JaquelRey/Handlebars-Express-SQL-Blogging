faea51b48fc77146344b6b5b3db963dcaa7b4bc2
jaquel <jaquelrey@gmail.com>
2022-11-05 17:33:04 -0400

    update: login / signup handler

--- public/js/mobile-nav.js
+++ public/js/mobile-nav.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elem = document.querySelectorAll('.modal');
-    M.Modal.init(elem);
-  });
-document.addEventListener('DOMContentLoaded', function() {
     const elem = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elem);
     // const instance = M.Sidenav.getInstance(elem);
